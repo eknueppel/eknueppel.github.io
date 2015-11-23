696c36f4019a887b661f388c672b609b7eb73ce4
Neutrino <edward.knueppel@zywave.com>
2015-11-23 15:38:23 -0800

    Fix for '/' issue

--- myScript.js
+++ myScript.js
@@ -204,7 +204,7 @@ function getRockCalc()
 {
 	var total = document.getElementById('rockTotal').value;
 	var interval = document.getElementById('rockInterval').value;
-	if(interval != 0)
+	if(interval != 0 && interval != null)
 	{
 	document.getElementById('rockCalc').value = total/interval;
 	}
