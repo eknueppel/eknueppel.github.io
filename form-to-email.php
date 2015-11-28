cb6fec16b0ad27219d4ce2effb636aa577b65680
Neutrino <edward.knueppel@zywave.com>
2015-11-28 08:03:15 -0800

    Create form-to-email.php

---
+++ form-to-email.php
@@ -0,0 +1,22 @@
+<?php
+  $name = $_POST['name'];
+  $visitor_email = $_POST['email'];
+  $message = $_POST['message'];
+
+    $email_from = 'edward.knueppel.jr@gmail.com';
+ 
+    $email_subject = "New Form submission";
+ 
+    $email_body = "You have received a new message from the user $name.\n".
+                            "Here is the message:\n $message".
+
+ 
+  $to = "edward.knueppel.jr@gmail.com";
+ 
+  $headers = "From: $email_from \r\n";
+ 
+  $headers .= "Reply-To: $visitor_email \r\n";
+ 
+  mail($to,$email_subject,$email_body,$headers);
+ 
+ ?>
